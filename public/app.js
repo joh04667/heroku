ba5c1900fd9c215153b602943de4eb185d0318ef
Kyle Johnson <joh04667@gmail.com>
2016-04-06 11:40:36 -0500

    be better

---
+++ public/app.js
@@ -0,0 +1,12 @@
+$(function() {
+
+ $('button').on('click', function() {
+   $('button').append("<h3>I'm making a note here: huge success");
+
+ });
+
+
+
+
+
+}); //
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express = require('express');
+var path = require('path');
+
 
 var app = express();
 
-
+app.use(express.static('public'));
 
 
 
@@ -10,4 +12,9 @@ var app = express();
 var server = app.listen(process.env.PORT || 3000, function(){
   var port = server.address().port;
   console.log('listening on port', port);
+});
+
+app.get('/', function(req, res) {
+  console.log('request incoming');
+  res.sendFile(path.join(__dirname, './index.html'));
 });
