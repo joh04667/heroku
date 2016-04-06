8f0ce159a99ce64ef78b40648a48f385d860f277
Kyle Johnson <joh04667@gmail.com>
2016-04-06 13:54:44 -0500

    be better

--- public/app.js
+++ public/app.js
@@ -4,7 +4,7 @@ $(function() {
  // pull out icky time stuff just get date
  $('.time').text(today.toString().match(/\w+ \w+ \d+ \d+/i));
 
- $.get("http://api.wunderground.com/api/18786a60dc483697/conditions/q/MN/Minneapolis.json").done(function(response) {
+ $.get("https://api.wunderground.com/api/18786a60dc483697/conditions/q/MN/Minneapolis.json").done(function(response) {
   var data = response.current_observation;
   $('.weather').append('<h3>'+ data.weather+'<h3>' + '<h4>' + data.temperature_string + '</h4>' + '<img src=' + data.image.url + ' />');
  });
